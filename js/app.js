c1decc73f455bb48c4b4e3a4859538c5b8149743
Russell Sabia <rjsabia@gmail.com>
2017-10-27 14:26:25 -0500

    added nav menu, andded nav id navigation, added app.js on scroll function, needs content and projects added, NEEDS responsive styling

--- js/app.js
+++ js/app.js
@@ -1,4 +1,14 @@
 $(document).ready( function() {
-	
+	$(window).bind('scroll', function() {
+	   var distance = 150;
+			 if ($(window).scrollTop() > distance) {
+				$('.nav-wrapper').addClass('scrolled');
+            	$('header').addClass('scrolled');
+			 }
+			 else {
+				$('.nav-wrapper').removeClass('scrolled');
+            	$('header').removeClass('scrolled');
+			 }
+	});
 })
 
