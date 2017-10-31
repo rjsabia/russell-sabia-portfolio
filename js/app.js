57a065c16966bb03cf0dbd236caf96a5cb9ab445
Russell Sabia <rjsabia@gmail.com>
2017-10-31 12:02:14 -0500

    added base carousel for work section, fixed coloring issue with top intro button

--- js/app.js
+++ js/app.js
@@ -1,6 +1,26 @@
 $(document).ready( function() {
+
+	$('.owl-carousel').owlCarousel({
+		  loop: true,
+		  margin: 10,
+		  nav: true,
+		  autoplay: true,
+		  autoplayHoverPause: true,
+		  responsive: {
+		    0: {
+		      items: 1
+		    },
+		    600: {
+		      items: 3
+		    },
+		    1000: {
+		      items: 3
+		    }
+		  }
+	})
+
 	$(window).bind('scroll', function() {
-	   var distance = 50;
+	   var distance = 60;
 			 if ($(window).scrollTop() > distance) {
 				$('.nav-wrapper').addClass('scrolled');
             	$('header').addClass('scrolled');
