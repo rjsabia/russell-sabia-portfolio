b722156af72e01e8eaaa27fda9c9be23a065e9be
Russell Sabia <rjsabia@gmail.com>
2017-10-31 19:26:17 -0500

    added meme porject to work section, adjust timing for intro animatiion to shorten

--- js/app.js
+++ js/app.js
@@ -1,24 +1,22 @@
 $(document).ready( function() {
-
 	$('.owl-carousel').owlCarousel({
-		  loop: true,
-		  margin: 10,
-		  nav: true,
-		  autoplay: true,
-		  autoplayHoverPause: true,
-		  responsive: {
-		    0: {
-		      items: 1
-		    },
-		    600: {
-		      items: 3
-		    },
-		    1000: {
-		      items: 3
-		    }
-		  }
+	  loop: true,
+	  margin: 15,
+	  nav: true,
+	  autoplay: true,
+	  autoplayHoverPause: true,
+	  responsive: {
+	    0: {
+	      items: 1
+	    },
+	    800: {
+	      items: 2
+	    },
+	    1000: {
+	      items: 3
+	    }
+	  }
 	})
-
 	$(window).bind('scroll', function() {
 	   var distance = 60;
 			 if ($(window).scrollTop() > distance) {
@@ -31,3 +29,10 @@ $(document).ready( function() {
 			 }
 	});
 })
+
+//  var angle = 0;
+// function galleryspin(sign) { 
+// spinner = document.querySelector("#spinner");
+// if (!sign) { angle = angle + 45; } else { angle = angle - 45; }
+// spinner.setAttribute("style","-webkit-transform: rotateY("+ angle +"deg); -moz-transform: rotateY("+ angle +"deg); transform: rotateY("+ angle +"deg);");
+// }
