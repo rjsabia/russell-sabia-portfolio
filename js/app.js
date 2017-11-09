5d3b357cf8946ae8fc0dd53b5e7de1026b9b4fbb
Russell Sabia <rjsabia@gmail.com>
2017-11-09 10:17:21 -0600

    added nav back, made minor adjustments for galaxy s5 responsive issue in ABOUT section

--- js/app.js
+++ js/app.js
@@ -21,4 +21,15 @@ $(document).ready( function() {
 	    }
 	  }
 	})
+	$(window).bind('scroll', function() {
+	   var distance = 40;
+			 if ($(window).scrollTop() > distance) {
+				$('.nav-wrapper').addClass('scrolled');
+            	$('header').addClass('scrolled');
+			 }
+			 else {
+				$('.nav-wrapper').removeClass('scrolled');
+            	$('header').removeClass('scrolled');
+			 }
+	});
 })
